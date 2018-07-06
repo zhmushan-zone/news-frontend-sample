662c34788adfdb0d3b5f8d7916b2f237bc25aa12
木杉 <zhbnh9988@163.com>
2018-07-06 11:04:25 +0800

    Update app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { NavMode } from './core/nav/nav.component';
+import { LoginDialogComponent } from './core/login-dialog/login-dialog.component';
+import { MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  constructor(
+    public dialog: MatDialog
+  ) { }
+
+  navClicked(mode: NavMode) {
+    switch (mode) {
+      case NavMode.login:
+        this.loginDialogOpen();
+        break;
+    }
+  }
+
+  loginDialogOpen() {
+    this.dialog.open(LoginDialogComponent);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { AppRtoutesModule } from './app.routes';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    SharedModule,
+    AppRtoutesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
