f24bbca7d453ce4cadefe17d78af9315df24447c
木杉 <zhbnh9988@163.com>
2018-07-07 17:15:44 +0800

    Add public assets

--- mock-server/src/main.ts
+++ mock-server/src/main.ts
@@ -1,8 +1,11 @@
 import * as koa from 'koa'
 import * as koaBodyparser from 'koa-bodyparser'
+import * as koaStatic from 'koa-static'
 import { router } from './app/routes'
+import * as path from 'path'
 
 const app = new koa()
+app.use(koaStatic(path.join(__dirname, '../public')))
 app.use(koaBodyparser())
 app.use(router.routes())
 app.listen(3000)
